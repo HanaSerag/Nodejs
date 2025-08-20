dfd074dc9967d935f6cdc0560cb91668a614de70
Hana Serag <hana.sultan36@gmail.com>
2025-08-20 13:37:40 +0300

    Delete Day6 directory

--- Day6/Data.js
+++
@@ -1,5 +0,0 @@
-let data = {
-    'Hana': { username: 'Hana', password: '12345678', email: 'hana@example.com' }
-};
-
-module.exports = data;
--- Day6/Events.js
+++
@@ -1,8 +0,0 @@
-const events = require('events');
-const rg = new events.EventEmitter();
-
-rg.on('Register', (data) => {
-    console.log('Register event received:', data);
-});
-
-module.exports = { rg };
