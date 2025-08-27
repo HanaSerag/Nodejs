d216c58f0c13646c9dabb43cd6e9ca1938357636
HanaSerag <hana.sultan36@gmail.com>
2025-08-27 23:51:07 +0300

    Rename and fix

--- Keber_Awy/index.js
+++ Keber_Awy/index.js
@@ -1,19 +1,22 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const mongoose = require('mongoose')
+const path = require('path');
 
+const connectDB = require('./config/connectDB');
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'public')));
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 connectDB();
+
 mongoose.connection.once('connected', () => {
-      console.log('Connected to MongoDB');
-      app.listen(process.env.port, () => {
-          console.log(`Server is running on http://localhost:${process.env.port}`);
-      });
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+    });
 });
 
-module.exports={app};
+module.exports = { app };
--- Keber_Awy/server.js
+++ Keber_Awy/server.js
@@ -1,30 +1,26 @@
-const {app}=require('./index');
-const{register,registerUser}=require('./controllers/register')
-const {findUser,login}=require('./controllers/login');
+const { app } = require('./index');
+const { register, registerUser } = require('./controllers/register');
+const { findUser, login } = require('./controllers/login');
 const { logout } = require('./controllers/logout');
-const {AddStudent,AddStudentPost,GETStudents,GETUsers,DeleteUser}=require('./controllers/addStudent')
-const {DeleteStudent}=require('./controllers/deleteStudent')
-const {EditStudent,EditStudentPost}=require('./controllers/editStudent')
+const { AddStudent, AddStudentPost, GETStudents, GETUsers } = require('./controllers/addStudent');
+const deleteUser = require('./controllers/delete-userController'); 
+const { editStudent, editStudentPost } = require('./controllers/editStudent');
 
-                                        
+app.get('/login', login);
+app.post('/login', findUser);
 
-app.get('/login',login);
-app.post('/login',findUser);
+app.get('/register', register);
+app.post('/register', registerUser);
 
-app.get('/register',register);
-app.post('/register',registerUser);
+app.get('/logout', logout);
 
-app.get('/logout',logout)
+app.get('/add-student', AddStudent);
+app.post('/add-student', AddStudentPost);
 
+app.get('/students', GETStudents);
+app.get('/users', GETUsers);
 
-app.get('/add-student',AddStudent)
-app.post('/add-student',AddStudentPost)
+app.post('/delete-user/:username', deleteUser); 
 
-app.get('/students',GETStudents)
-app.get('/users',GETUsers)
-
-app.post('/delete-user/:id',DeleteUser)
-
-app.get('/editStudent/:id',editStudent)
-app.post('/editStudent/:id',editStudentPost)
-app.post('/deleteStudent/:id',deleteStudent)
+app.get('/editStudent/:id', editStudent);
+app.post('/editStudent/:id', editStudentPost);
